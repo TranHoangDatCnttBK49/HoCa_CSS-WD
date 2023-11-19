0be89d738ad700fd3644cc6028cddf3cd2987e15
TranHoangDatCnttBK49 <148096333+TranHoangDatCnttBK49@users.noreply.github.com>
2023-11-20 01:32:06 +0700

    first commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,16 @@
+/* Trần Hoàng Phát */
+function myFunction1() {
+    document.body.style.backgroundImage = "url('assets/img/backgroundcoral1.jpg')";
+    document.body.style.rowGap = 'no-repeat';
+    document.body.style.attachment = 'fixed';
+    document.body.style.size = '100% 100%';
+  }
+function myFunction2() {
+    document.body.style.backgroundImage = "url('assets/img/backgroundcoral3.jpg')";
+    document.body.style.rowGap = 'no-repeat';
+    document.body.style.attachment = 'fixed';
+    document.body.style.size = '100% 100%';
+  }
+/* Trần Hoàng Phát */
+
+
